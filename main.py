dd38656b297c7a6d65a13363c626bb22ae261c5e
Simon <gtrsimon@gmail.com>
2019-04-17 16:15:51 -0400

    Added functionality

--- main.py
+++ main.py
@@ -6,16 +6,27 @@ current_profile = {}
 
 # Current Profile Global Var Helpers
 
+def update_weight():
+    global current_profile
+    current_profile['currentWeight'] = request_input('Let\'s update your profile. What is your current weight?: ')
+    # Do not save until they have completed the workout
+
+
 def update_day():
     global current_profile
     current_profile['day'] = current_profile['day'] + 1
-    save_profile()
+    # Do not save until they have completed the workout
 
 
 def update_last_muscle_group(last_muscle_group):
     global current_profile
     current_profile['lastMuscleGroup'] = last_muscle_group
-    save_profile()
+    # Do not save until they have completed the workout
+
+
+def open_profile():
+    global current_profile
+    current_profile = ioService.open_file('profile')
 
 
 def save_profile():
@@ -24,7 +35,14 @@ def save_profile():
 
 def create_new_profile():
     global current_profile
-    current_profile = {'day': 1, 'lastMuscleGroup': ''}
+
+    print('Before we start, PyFit needs some information from you... \n')
+    current_weight = request_input('What is your current weight (in lbs)? ')
+    print('')
+    target_weight = request_input('What is your target weight (in lbs)? ')
+    print('')
+
+    current_profile = {'day': 1, 'lastMuscleGroup': '', 'currentWeight': current_weight, 'targetWeight': target_weight}
     save_profile()
 
 
@@ -35,7 +53,29 @@ def request_input(prompt):
     return value
 
 
-def request_workout(category, category_id):
+def request_exercise_status():
+    selection = request_input('Are you finished this exercise? (y/n): ')
+    if selection.lower() == 'y':
+        print('Woohoo! Congrats on making it through another day in the challenge!')
+        save_profile()
+    elif selection.lower() == 'n':
+        print('Keep going!')
+        print()
+        request_exercise_status()
+    else:
+        print()
+        request_exercise_status()
+
+def request_workout_options(exercise):
+    print('Great choice! Here is a quick description of the workout:')
+    print(exercise['description'])
+    request_exercise_status()
+
+
+    # Update profile when workout is complete
+
+
+def request_workout(category_id):
     print('Here are some workouts you can choose from, select one: \n')
     raw_exercises = restService.get_exercises_by_category(category_id)['results']
     exercises = {}
@@ -48,14 +88,10 @@ def request_workout(category, category_id):
 
     requested_exercise = request_input('\nYour selection: ')
     if requested_exercise.lower() in exercises:
-        print('Exercise found!')
-        update_last_muscle_group(category)
-        update_day()
+        request_workout_options(exercises[requested_exercise.lower()])
     else:
         print('\nPlease enter in a valid exercise!')
-        request_workout(category, category_id)
-
-    # TODO: Next, we will ask them if they need more information for the workout
+        request_workout(category_id)
 
 
 def request_category():
@@ -71,7 +107,8 @@ def request_category():
 
     requested_category = request_input('\nYour selection: ')
     if requested_category.lower() in categories:
-        request_workout(requested_category, categories[requested_category.lower()])
+        request_workout(categories[requested_category.lower()])
+        update_last_muscle_group(requested_category)
     else:
         print('\nPlease enter in a valid muscle group:')
         request_category()
@@ -79,11 +116,12 @@ def request_category():
 
 def determine_day_in_challenge():
     if ioService.does_file_exist('profile'):
-        global current_profile
-        current_profile = ioService.open_file('profile')
+        open_profile()
         print('Welcome back to the challenge! You are on Day', current_profile['day'])
+        print('Your target weight is:', current_profile['targetWeight'], 'lbs')
         if current_profile['lastMuscleGroup']:
             print('Your last exercised muscle group:', current_profile['lastMuscleGroup'], '\n')
+        update_day()
     else:
         print('Are you ready to start the challenge? Welcome to Day 1!')
         create_new_profile()
